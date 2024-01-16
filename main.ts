1b1bd2bf7a81405fec77088d82c8c8afe114a712
dcolumbus2470 <156682238+dcolumbus2470@users.noreply.github.com>
2024-01-16 12:21:47 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let start_time = 0
+let stop_time = 0
+fwdSensors.touch.fwdOnTouch(jacdac.ButtonEvent.Down, function () {
+    fwdMotors.middleServo.fwdSetSpeed(0)
+})
+fwdSensors.dial1.fwdOnDialTurned(fwdSensors.DialDirection.CCW, function (difference) {
+    fwdMotors.middleServo.fwdSetSpeed(fwdSensors.dial1.fwdPosition())
+})
+fwdSensors.dial1.fwdOnDialTurned(fwdSensors.DialDirection.CW, function (difference) {
+    fwdMotors.middleServo.fwdSetSpeed(fwdSensors.dial1.fwdPosition())
+})
+input.onButtonPressed(Button.A, function () {
+    start_time = input.runningTime()
+})
+input.onButtonPressed(Button.B, function () {
+    stop_time = input.runningTime()
+    basic.showNumber((stop_time - start_time) / 1000)
+})
